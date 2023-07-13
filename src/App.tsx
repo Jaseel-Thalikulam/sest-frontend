f2cf878ab7f69f5366743a10cb98435fcf41f82e
Jaseel <jaseelta1@gmail.com>
2023-07-13 23:44:58 +0530

    minor updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 
 
 import './App.scss'
-import Register from './Components/Register/Register'
-import Header from './Components/Header/Header'
+
+
 
 function App() {
 
 
   return (
     <>
-    <Header/>
+    
     </>
   )
 }
