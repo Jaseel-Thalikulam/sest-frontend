d6765ecebd14b8e7a5457fed110cf62391de1704
Jaseel <jaseelta1@gmail.com>
2023-08-10 17:52:01 +0530

    conflits fixed

--- src/tutor/pages/Profile/Profile.tsx
+++ src/tutor/pages/Profile/Profile.tsx
@@ -17,9 +17,6 @@ import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import { UserDetails } from '../../../redux/userSlice/UserSlice';
 import APIResponse from '../../../interface/IaddUserDetails/IaddUserDetails';
-const BASE_URL:string = import.meta.env.VITE_BACKEND_BASE_URL as stringimport axios from 'axios';
-import { ToastContainer, toast } from 'react-toastify';
-import { UserDetails } from '../../../redux/userSlice/UserSlice';
 const BASE_URL:string = import.meta.env.VITE_BACKEND_BASE_URL as string
 
 
@@ -55,7 +52,7 @@ const ProfilePage: React.FC = () => {
     return date.toLocaleDateString("en-GB", options);
   }
 
-  const { name, email, phoneNumber, role, about, DOB, URLs, tags,_id,avatarUrl,_id,avatarUrl } = data;
+  const { name, email, phoneNumber, role, about, DOB, URLs, tags,_id,avatarUrl } = data;
 
 
   console.log(URLs)
@@ -81,8 +78,74 @@ const ProfilePage: React.FC = () => {
     const file = event.target.files?.[0];
     if (file) {
 
+      
+      const formData = new FormData();
+      formData.append('image', file);
+      formData.append('userId', _id);
+      
+      const response:{data:APIResponse} = await axios.post(
+        `${BASE_URL}/upload/avatar`,
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data', // Important for sending files
+          },
+         
+        }
+      );
+  
+      const { success, message, userData } = response.data
+     
+      if (success) {
+        const URLs = userData.URLs
 
-      });
+      if (URLs) {
+        dispatch(
+          UserDetails({
+            role: userData.role,
+            name: userData.name,
+            email: userData.email,
+            phoneNumber: userData.phoneNumber,
+            DOB: userData.DOB,
+            _id: userData._id,
+            about: userData.about,
+            isBanned:userData.isBanned,
+            URLs: {
+              github: URLs.github,
+              linkedin: URLs.linkedin,
+              pinterest: URLs.pinterest,
+            },
+            tags: userData.tags,
+           avatarUrl:userData.avatarUrl
+          })
+        )
+      } else {
+        dispatch(
+          UserDetails({
+            role: userData.role,
+            name: userData.name,
+            email: userData.email,
+            phoneNumber: userData.phoneNumber,
+            DOB: userData.DOB,
+            _id: userData._id,
+            about: userData.about,
+            isBanned:userData.isBanned,
+            URLs: {
+              github: '',
+              linkedin: '',
+              pinterest: '',
+            },
+            tags: userData.tags,
+            avatarUrl:userData.avatarUrl
+          })
+        )
+      }
+        toast.success(message)
+      }else{
+        toast.error(message)
+
+      }
+   
     }
   }
   
