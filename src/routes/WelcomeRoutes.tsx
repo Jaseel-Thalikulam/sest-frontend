cce10509f6732274a6ac960dfdac2e8e68392fb1
Jaseel <jaseelta1@gmail.com>
2023-10-06 19:30:29 +0530

    added Lazy & Suspense in Welcome Routes

--- src/routes/WelcomeRoutes.tsx
+++ src/routes/WelcomeRoutes.tsx
@@ -1,29 +1,55 @@
 import WelcomeLayout from "../common/layouts/WelcomeLayout";
 import  {lazy, Suspense} from 'react';
 import { Route, Routes } from "react-router-dom";
-const LoadingLandingPage = lazy(() => import('../common/pages/LandingPage/LandingPage'));
 import UserPublicRoute from "../authRoutes/UserPublicRoute";
 import Loading from "../common/Components/loadingComponent/Loading";
-import CourseDetailPage from "../common/pages/CourseDetailPage/CourseDetailPage";
+
+
+
+const CourseDetailPage = lazy(() => import('../common/pages/CourseDetailPage/CourseDetailPage'));
+const LoadingLandingPage = lazy(() => import('../common/pages/LandingPage/LandingPage'));
 const LoadingEducatorWelcomePage =lazy(()=>import('../common/pages/EducatorWelcomePage/EducatorWelcomePage'))
 function WelcomeRoutes() {
   return (
       <>
-          <WelcomeLayout>
-              <Routes>
+      <WelcomeLayout>
+        <Routes>
+          
                   
-        <Route  path='/' element={<UserPublicRoute><Suspense fallback={<Loading/>}>
+          <Route path='/' element={<UserPublicRoute><Suspense fallback={<Loading />}>
+            
             <LoadingLandingPage />
-          </Suspense></UserPublicRoute>} />
-        <Route  path='/welcome/educator' element={<UserPublicRoute><Suspense fallback={<Loading/>}>
-            <LoadingEducatorWelcomePage />
-          </Suspense></UserPublicRoute>} />
+
+          </Suspense>
+            
+          </UserPublicRoute>} />
+
+
+          <Route path='/welcome/educator' element={<UserPublicRoute>
+            
+            <Suspense fallback={<Loading />}>
+
+              <LoadingEducatorWelcomePage />
+              
+            </Suspense>
+
+          </UserPublicRoute>} />
+
+          
           <Route path='/course/:CourseId' element={<UserPublicRoute>
-          <CourseDetailPage />
-        </UserPublicRoute>}/>
-        </Routes>
+
+            <Suspense fallback={<Loading />}>
+              
+              <CourseDetailPage />
+            
+            </Suspense>
+
+          </UserPublicRoute>} />
+
         
-          </WelcomeLayout>
+        </Routes>
+      </WelcomeLayout>
+      
     </>
   );
 }
